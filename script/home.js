66e6666cc3f3973c97a1d89456e6bee68505eda0
J-Rathman <jonathanrathman2004@gmail.com>
2023-12-07 15:12:29 -0500

    added logos, replaced some placeholders

--- script/home.js
+++ script/home.js
@@ -1,11 +1,17 @@
+
+//document.getElementsByClassName("logo-link")[0].addEventListener("click", () => window.location.href = "index.html");
+
+for (let element of document.getElementsByClassName("logo-link")) {
+    element.addEventListener("click", () => window.location.href = "index.html");
+}
+
+function goHome() {
+    window.location.href = "index.html";
+}
+
 function orderPopup() {
     document.getElementsByClassName("order-popup")[0].classList.remove("hidden");
     document.getElementsByClassName("main-content")[0].classList.add("hidden");
     window.scrollTo(0, 0);
 }
 
-let cart = [];
-function addHighlight(item) {
-    window.location = "order.html";
-    cart.push(item);
-}
